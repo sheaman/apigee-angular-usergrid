67c086030e82127fce2b2112734de0c5f7ea878d
Wesley Hales <wesleyhales@gmail.com>
2013-01-29 18:19:56 -0600

    cleanup and better object detection for dynamic data

--- usergrid/examples/all-calls/angular-app.js
+++ usergrid/examples/all-calls/angular-app.js
@@ -1,4 +1,13 @@
 
+//var usergrid_request = {
+//  username:"fred",
+//  password:"barney",
+//  orgName:"yourorgname",
+//  appName:"yourappname",
+//  endpointName:"users",
+//  grant_type: "password"
+//};
+
 var usergrid_request = {
   username:"demo",
   password:"user123",
@@ -14,7 +23,7 @@ angular.module('project', ['usergrid']).
       when('/', {controller:ListCtrl, templateUrl:'list.html'}).
       otherwise({redirectTo:'/'});
   });
-var inputValue = true;
+
 function ListCtrl($scope, Project) {
   $scope.projects = refreshData();
   var refreshInput;
@@ -30,45 +39,57 @@ function ListCtrl($scope, Project) {
 
   }
 
+  $scope.isDeep = function(item){
+    //can be better, just see if object for now
+    return (Object.prototype.toString.call(item) === "[object Object]");
+  };
+
+  var auth_token;
   function refreshData(){
-    var auth_token;
+
     if(typeof(Storage)!=="undefined" && localStorage.getItem('token12')) {
       auth_token = localStorage.getItem('token12');
     }
      return Project($scope.orgName,$scope.appName,$scope.endpointName).get({'access_token':auth_token}, angular.noop,
        function(responseFail){
+//         console.log('responseSuccess----',responseFail.status,responseFail);
        if(responseFail.status === 401){
          //we reached the request limit, so use account login
-          auth = Project(usergrid_request.orgName,
-                         usergrid_request.appName,
-                                              "token").get({
-                                                          'username': encodeURIComponent(usergrid_request.username),
-                                                          'password': usergrid_request.password,
-                                                          'grant_type':usergrid_request.grant_type
-                                                          }, function(loginResponseSuccess) {
-
-              auth_token = loginResponseSuccess.access_token;
-
-              if(typeof(Storage)!=="undefined" && auth_token!=="undefined"){
-                localStorage.setItem('token12', auth_token);
-              }
-            }, function(loginResponseFail) {
-              console.log('loginResponseFail----',loginResponseFail.status,loginResponseFail.data);
-            });
-         }else{
+           console.log('login');
+           loginUser();
+       }else{
          //login failed for some other reason, maybe stale token?
          localStorage.setItem('token12', undefined);
            if(responseFail.status !== 400){
               refreshData(); //try again
            }
-         }
+       }
 
      }, function(responseSuccess) {
 
-       console.log('responseSuccess----',responseSuccess.status,responseSuccess);
+//       console.log('responseSuccess----',responseSuccess.status,responseSuccess);
      });
     //  $scope.projects = Project.get({'userId': 123}, function() {});
     //  $scope.projects = Project.query(); only use query when returns array
+  }
+
+  function loginUser(){
+    auth = Project(usergrid_request.orgName,
+      usergrid_request.appName,
+      "token").get({
+        'username': encodeURIComponent(usergrid_request.username),
+        'password': usergrid_request.password,
+        'grant_type':usergrid_request.grant_type
+      }, function(loginResponseSuccess) {
+
+        auth_token = loginResponseSuccess.access_token;
+
+        if(typeof(Storage)!=="undefined" && auth_token!=="undefined"){
+          localStorage.setItem('token12', auth_token);
+        }
+      }, function(loginResponseFail) {
+        console.log('loginResponseFail----',loginResponseFail.status,loginResponseFail.data);
+      });
   }
 
 }
@@ -87,61 +108,3 @@ angular.module('usergrid', ['ngResource']).
         });
     }
 });
-
-function debounce(func, wait) {
-  var timer;
-  return function() {
-    var context = this, args = arguments;
-    clearTimeout(timer);
-    timer = setTimeout(function() {
-      timer = null;
-      func.apply(context, args);
-    }, wait);
-  };
-}
-
-//app.factory('debounce', function($timeout, $q) {
-//  return function(func, wait, immediate) {
-//    var timeout;
-//    var deferred = $q.defer();
-//    return function() {
-//      var context = this, args = arguments;
-//      var later = function() {
-//        timeout = null;
-//        if(!immediate) {
-//          deferred.resolve(func.apply(context, args));
-//          deferred = $q.defer();
-//        }
-//      };
-//      var callNow = immediate && !timeout;
-//      if ( timeout ) {
-//        $timeout.cancel(timeout);
-//      }
-//      timeout = $timeout(later, wait);
-//      if (callNow) {
-//        deferred.resolve(func.apply(context,args));
-//        deferred = $q.defer();
-//      }
-//      return deferred.promise;
-//    };
-//  };
-//});
-//
-//app.controller('MainCtrl', function($scope, debounce) {
-//  $scope.msgs = [];
-//  $scope.addMsg = function(msg) {
-//    $scope.msgs.push(msg);
-//    return msg;
-//  };
-//
-//  $scope.addMsgDebounced = debounce($scope.addMsg, 2000, false);
-//
-//  $scope.logReturn = function(msg) {
-//    var returned = $scope.addMsgDebounced(msg);
-//    console.log('Log: ', returned);
-//    returned.then(function(value) {
-//      console.log('Resolved:', value);
-//    });
-//  };
-//
-//});
